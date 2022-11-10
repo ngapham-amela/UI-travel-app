52166a10662a8733f4d5cc0744d8d904996cf5a3
nga.pham <nga.pham@amela.vn>
2022-11-10 17:36:11 +0700

    init intro screen

--- lib/representation/screens/splash_screen.dart
+++ lib/representation/screens/splash_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:travel_app_flutter/core/helpers/asset_helper.dart';
 import 'package:travel_app_flutter/core/helpers/image_helper.dart';
+import 'package:travel_app_flutter/representation/screens/intro_screen.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -12,6 +13,17 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreen extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    redirectIntroScreen();
+  }
+
+  void redirectIntroScreen() async {
+    await Future.delayed(const Duration(seconds: 2));
+    Navigator.of(context).pushNamed(IntroScreen.routeName);
+  }
+
   @override
   Widget build(BuildContext context) {
     // use Stack() to stack blocks
---
+++ lib/representation/widgets/item_button_widget.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:travel_app_flutter/core/constants/color_constants.dart';
+import 'package:travel_app_flutter/core/constants/dismension_constants.dart';
+import 'package:travel_app_flutter/core/constants/textstyle_ext.dart';
+
+class ItemButtonWidget extends StatelessWidget {
+  const ItemButtonWidget({Key? key, required this.data, this.onTap, this.color})
+      : super(key: key);
+
+  final String data;
+  final Function()? onTap;
+  final Color? color;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        padding: const EdgeInsets.symmetric(vertical: kDefaultPadding),
+        width: double.infinity,
+        decoration: BoxDecoration(
+          color: color,
+          borderRadius: BorderRadius.circular(kMediumPadding),
+          gradient: Gradients.defaultGradientBackground,
+        ),
+        alignment: Alignment.center,
+        child: Text(
+          data,
+          style: color == null
+              ? TextStyles.defaultStyle.whiteTextColor.bold
+              : TextStyles.defaultStyle.bold.copyWith(
+                  color: ColorPalette.primaryColor,
+                ),
+        ),
+      ),
+    );
+  }
+}
