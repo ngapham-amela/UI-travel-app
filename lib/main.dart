43db6c833c3570518323452902bda03ed43e5144
nga.pham <nga.pham@amela.vn>
2022-11-11 14:29:38 +0700

    logic remove Introduction Page

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:travel_app_flutter/core/constants/color_constants.dart';
+import 'package:travel_app_flutter/core/helpers/local_storage_helper.dart';
 import 'package:travel_app_flutter/representation/screens/splash_screen.dart';
 import 'package:travel_app_flutter/routers.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
-void main() {
+void main() async {
+  await Hive.initFlutter();
+  await LocalStorageHelper.initLocalStorageHelper();
   runApp(const MyApp());
 }
 
@@ -13,7 +17,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Travel App',
       theme: ThemeData(
         primaryColor: ColorPalette.primaryColor,
         backgroundColor: ColorPalette.backgroundScaffoldGold,
